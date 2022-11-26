b7190927907b2ec4b325f09b53342165b6d08a92
hamid <74140209+hamid423@users.noreply.github.com>
2022-11-26 11:37:29 +0100

    first commit

---
+++ src/main/java/com/example/backend/entities/BankAccount.java
@@ -0,0 +1,25 @@
+package com.example.backend.entities;
+
+import com.example.backend.enums.AccountStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Inheritance
+@Data @AllArgsConstructor @NoArgsConstructor
+public class BankAccount {
+    @Id
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
+    private List<AccountOperation> accountOperations;
+}
